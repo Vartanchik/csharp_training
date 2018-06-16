bde0901db6541f66485e0e16625bd10b3f04b1d2
HAVG <HAVG@HAVG-PC>
2018-06-16 17:59:55 +0300

    Add LoginTests

--- addressbook-web-tests/addressbook-web-tests/appmanager/HelperBase.cs
+++ addressbook-web-tests/addressbook-web-tests/appmanager/HelperBase.cs
@@ -20,12 +20,25 @@ namespace WebAddressBookTests
             driver = manager.Driver;
         }
 
-        protected void Type(By locator, string text)
+        public void Type(By locator, string text)
         {
             if (text != null)
             {
                 driver.FindElement(locator).Clear();
                 driver.FindElement(locator).SendKeys(text);
+            }
+        }
+
+        public bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
             }
         }
     }
---
+++ addressbook-web-tests/addressbook-web-tests/tests/LoginTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebAddressBookTests
+{
+    [TestFixture]
+    public class LoginTests : TestBase
+    {
+        [Test]
+        public void LoginWithValidCredentialsTest()
+        {
+            app.Auth.Logout();
+
+            AccountData account = new AccountData("admin", "secret");
+            app.Auth.Login(account);
+
+            Assert.IsTrue(app.Auth.IsLoggedIn(account));
+        }
+
+        [Test]
+        public void LoginWithInvalidCredentialsTest()
+        {
+            app.Auth.Logout();
+
+            AccountData account = new AccountData("admin", "12354");
+            app.Auth.Login(account);
+
+            Assert.IsFalse(app.Auth.IsLoggedIn(account));
+        }
+    }
+}
