c15ae9c2e04a80034e798837fa2a148fa9d1b851
HAVG <HAVG@HAVG-PC>
2018-07-09 22:49:16 +0300

    Lesson 6.4 Додав запис та зчитування данних з excel.

--- addressbook-web-tests/addressbook-test-data-generators/Program.cs
+++ addressbook-web-tests/addressbook-test-data-generators/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
+using Excel = Microsoft.Office.Interop.Excel;
 using WebAddressBookTests;
 
 namespace addressbook_test_data_generators
@@ -16,11 +17,10 @@ namespace addressbook_test_data_generators
         static void Main(string[] args)
         {
             int count =  Convert.ToInt32(args[0]);
-            StreamWriter writer = new StreamWriter(args[1]);
+            string fileName = args[1];
             string format = args[2];
 
             List<GroupData> groups = new List<GroupData>();
-
             for (int i = 0; i < count; i++)
             {
                 groups.Add(new GroupData(TestBase.GenerateRandomString(10))
@@ -31,23 +31,57 @@ namespace addressbook_test_data_generators
 
             }
 
-            if (format == "csv")
+            if (format == "excel")
             {
-                writeGroupsToCsvFile(groups, writer);
-            }
-            else if (format == "xml")
-            {
-                writeGroupsToXmlFile(groups, writer);
-            }
-            else if (format == "json")
-            {
-                writeGroupsToJsonFile(groups, writer);
+                writeGroupsToExcelFile(groups, fileName);
             }
             else
             {
-                System.Console.Out.Write("Unrecognized format {0}", format);
+                StreamWriter writer = new StreamWriter(fileName);
+                if (format == "csv")
+                {
+                    writeGroupsToCsvFile(groups, writer);
+                }
+                else if (format == "xml")
+                {
+                    writeGroupsToXmlFile(groups, writer);
+                }
+                else if (format == "json")
+                {
+                    writeGroupsToJsonFile(groups, writer);
+                }
+                else
+                {
+                    System.Console.Out.Write("Unrecognized format {0}", format);
+                }
+                writer.Close();
             }
-            writer.Close();
+        }
+
+        static void writeGroupsToExcelFile(List<GroupData> groups, string fileName)
+        {
+            Excel.Application app = new Excel.Application();
+            app.Visible = true;
+            Excel.Workbook wb = app.Workbooks.Add();
+            Excel.Worksheet sheet = wb.ActiveSheet;
+            sheet.Cells[1, 1] = "test";
+
+            int row = 1;
+            foreach(GroupData group in groups)
+            {
+                sheet.Cells[row, 1] = group.Name;
+                sheet.Cells[row, 2] = group.Header;
+                sheet.Cells[row, 3] = group.Footer;
+                row++;
+            }
+
+            string fullPath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+            File.Delete(fullPath);
+            wb.SaveAs(fullPath);
+
+            wb.Close();
+            app.Visible = false;
+            app.Quit();
         }
 
         static void writeGroupsToCsvFile(List<GroupData> groups, StreamWriter writer)
